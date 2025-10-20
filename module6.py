3b0bcebd1b781d9cef55b3745815b508f1c92b6d
janesha008 <jane.sha008@gmail.com>
2025-10-20 18:15:10 +0530

    Success

---
+++ module6.py
@@ -0,0 +1,21 @@
+class Beans:
+    def type(self):
+        print("Vegetable")
+    def color(self):
+        print("Green")
+
+class Mango:
+    def type(self):
+        print("Fruit")
+    def color(self):
+        print("Yellow")
+
+def func(obj):
+    obj.type()
+    obj.color()
+
+beans = Beans()
+mango = Mango()
+
+func(beans)
+func(mango)
